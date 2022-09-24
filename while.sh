eb1331b8cac4bb7249000cde2c67cfba740b0b6e
gleehave <gleehave@gmail.com>
2022-09-24 17:22:31 +0900

    [add] while.sh

---
+++ while.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+index=0
+while [ $index -lt 10 ]
+do
+	echo "index is $index"
+	((index++))
+	echo "now index is $index"
+done
+echo "Done"
+
+index=0
+while [ $index -lt 10 ]
+do
+	echo "index is $index"
+	while [ .. ]
+	do
+		if [ ... ]; then
+			continue
+		fi
+	done
+	((index++))	
+done
+echo
